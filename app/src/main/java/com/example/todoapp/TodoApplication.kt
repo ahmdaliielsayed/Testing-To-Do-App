8a542ca876540625cb4c6063f9ba53f9efceafae
SharafElDinA <ahmed.sharafeldin@vodafone.com>
2023-05-28 20:46:11 +0300

    first commit to upload To Do App

---
+++ app/src/main/java/com/example/todoapp/TodoApplication.kt
@@ -0,0 +1,12 @@
+package com.example.todoapp
+
+import android.app.Application
+import timber.log.Timber
+
+class TodoApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+}
---
+++ app/src/main/java/com/example/todoapp/statistics/StatisticsFragment.kt
@@ -0,0 +1,43 @@
+package com.example.todoapp.statistics
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.todoapp.R
+import com.example.todoapp.databinding.StatisticsFragBinding
+import com.example.todoapp.util.setupRefreshLayout
+
+/**
+ * Main UI for the statistics screen.
+ */
+class StatisticsFragment : Fragment() {
+
+    private lateinit var viewDataBinding: StatisticsFragBinding
+
+    private val viewModel by viewModels<StatisticsViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        viewDataBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.statistics_frag,
+            container,
+            false
+        )
+        return viewDataBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewDataBinding.viewmodel = viewModel
+        viewDataBinding.lifecycleOwner = this.viewLifecycleOwner
+        this.setupRefreshLayout(viewDataBinding.refreshLayout)
+    }
+}
